0602711fe7d7a46680c7b72903203c77c49d8b44
Ivan Sennov <3aklinatel@gmail.com>
2020-09-28 03:12:18 +0800

    remove excess comma from config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,8 +19,7 @@ module.exports = {
       ref  : process.env.DEPLOY_BRANCH || 'master',
       repo : process.env.DEPLOY_GIT || 'git@github.com:Zaklinatel/di.scord.git',
       path : process.env.DEPLOY_PATH,
-      'post-deploy' : 'npm i --production; tsc & pm2 restart ecosystem.prod.config.js',
-
+      'post-deploy' : 'npm i --production; tsc & pm2 restart ecosystem.prod.config.js'
     }
   }
 };
